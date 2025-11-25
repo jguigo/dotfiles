43616ca773bd8b6cfa7ea0678438f405f1c00785
jguigo <jguilhermefmelo21@gmail.com>
2025-11-25 10:27:37 -0300

    uses `grimblast`

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 output_file="$HOME/media/screenshots/screenshot_$(date +'%Y-%m-%d-%H%M%S').png"
-grim -g "$(slurp)" $output_file && wl-copy < $output_file
+grimblast copysave area $output_file
