0f0d37f207169f488aacf3eb9c2af6ef64456252
jguigo <jguilhermefmelo21@gmail.com>
2025-12-23 10:28:10 -0300

    adds `diagnostics`

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,18 +1,18 @@
-require("lualine").setup({
+require('lualine').setup {
   options = {
-    theme = "auto",
-    section_separators = { left = "", right = "" },
-    component_separators = { left = "", right = "" },
+    theme = 'auto',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
     icons_enabled = true,
   },
 
   sections = {
-    lualine_a = { "mode" },                   -- NORMAL / INSERT / VISUAL
-    lualine_b = { "branch", "diff" },         -- git info
-    lualine_c = { "filename" },               -- current file
+    lualine_a = { 'mode' }, -- NORMAL / INSERT / VISUAL
+    lualine_b = { 'branch', 'diff', 'diagnostics' }, -- git info
+    lualine_c = { 'filename' }, -- current file
 
-    lualine_x = { "encoding", "filetype" },
-    lualine_y = { "progress" },               -- progress %
-    lualine_z = { "location" },               -- line:column
+    lualine_x = { 'encoding', 'filetype' },
+    lualine_y = { 'progress' }, -- progress %
+    lualine_z = { 'location' }, -- line:column
   },
-})
+}
