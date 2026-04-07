47a5a63b4bd5d0c17a06a8575a4179ba131e149e
jguigo <jguilhermefmelo21@gmail.com>
2026-04-07 14:34:22 -0300

    fix: `treesitter` highlight problem

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,34 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		event = { "BufReadPost", "BufNewFile" },
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"html",
-					"css",
-					"javascript",
-					"typescript",
-					"tsx",
-					"json",
-					"markdown",
-					"markdown_inline",
-					"bash",
-					"go",
-					"c",
-					"diff",
-				},
-				auto_install = true,
-				sync_install = false,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				indent = { enable = true },
+			require("nvim-treesitter").install({
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"go",
+				"c",
+				"diff",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true }),
+				callback = function()
+					pcall(vim.treesitter.start)
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
 			})
 		end,
 	},
